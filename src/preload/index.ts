ea05b6f7f8fa12658cf95b1c99f95e16c005453c
Frank515 <frank515_@outlook.com>
2023-11-26 15:31:53 +0800

    add close api

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,7 +2,11 @@ import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  close: () => {
+    electronAPI.ipcRenderer.send("close")
+  }
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
